eecdf2cccf5f8b0b57dbd8535d6b35cfa79ea2ca
Ali Mollaahmadi Dehaqi <a9473m@gmail.com>
2020-11-09 19:22:09 +0330

    adding templates
    adding checkout, store and product details templates

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('product/', views.product), #<int:pk>
+    path('checkout/', views.checkout),
+    path('store/', views.store),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
     template_name = "templates/index.html"
     return render(request,template_name)
+
+def product(request):
+    template_name = "templates/product.html"
+    return render(request,template_name)
+
+def checkout(request):
+    template_name = "templates/checkout.html"
+    return render(request,template_name)
+
+def store(request):
+    template_name = "templates/store.html"
+    return render(request,template_name)
