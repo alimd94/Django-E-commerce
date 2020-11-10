77ea1a77679635c232450f93d5df73b3f51b92fe
Ali Mollaahmadi Dehaqi <a9473m@gmail.com>
2020-11-10 20:45:14 +0330

    database v1
    1) Added database schema file as a PNG file for better 
    understanding 
    
    2) Added model Skeleton and their fields
    
    3) Change database back-end from Sqlite to PostgreSQL

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+# # Create your models here.
+# class Category():
+#     name
+#     parent
+
+# class Product(models.Model):
+#     name
+#     weight 
+#     dimension
+#     description
+#     category
+#     created_at
+#     model_name
+#     brand
+#     image
+
+# class Product_Detalis(models.Model):
+#     product
+#     color
+#     price
+#     has_Guaranty
+#     quantity
+
+# class Promotions(models.Model):
+#     product_Details
+#     start_Date
+#     end_Date
+#     percentage
+
+# class User():
+#     first_name
+#     last_name
+#     email
+#     phone_Number
+#     password
+#     addresses
+#     created_at
+#     updated_at
+#     is_active
+
+# class Reviews(models.Model):
+#     user
+#     title
+#     body
+#     created_at
+#     product
+#     rating
+
+# class Favorites(models.Model):
+#     user
+#     product
+
+# class Address(models.Model):
+#     user
+#     full_Address
+#     is_Personal
+
+# class Order(models.Model):
+#     product_Details
+#     user
+#     address
+#     quantity
+#     price
+#     created_at
+#     status
+#     description
