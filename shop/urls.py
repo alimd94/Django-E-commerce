ba39944ba010096f6b030c06deb9209813a7f974
Ali Mollaahmadi Dehaqi <a9473m@gmail.com>
2020-12-05 19:45:36 +0330

    working on hompage
    top selling by categories  and new product by categories added to homepage
    { product and their attributes}

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "shop"
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    path('product/<int:pk>/', views.ProductDetail.as_view()), #<int:pk>
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='detail'), #<int:pk>
     path('checkout/', views.checkout),
     path('store/', views.store, name="store"),
 ]
